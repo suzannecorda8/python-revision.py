6dd0a3a6ca804aa5495208bad5a0eb246d2c0777
suzannecorda8 <103393836+suzannecorda8@users.noreply.github.com>
2022-09-09 20:09:57 +0530

    Add files via upload

---
+++ revision.py
@@ -0,0 +1,45 @@
+name="Suzanne Corda"
+birth_year=input('Birth year:')
+age=2022-int(birth_year)
+print(age)
+
+weight_lbs=input('weight(lbs):')
+weight_kg=float(weight_lbs)*0.45
+print(weight_kg)
+course='''So we are pleased to inform you that we have introduced a new course for you all.I hope this course will help you learn a lot of new things '''
+print(course)
+coursee='PYTHON FOR CODIng'
+print(coursee[:4])
+print(coursee.upper())
+print(coursee.lower())
+print(coursee.find('P'))
+# print(course.append('eelle'))
+print(len(coursee))
+print(coursee.replace('P','J'))
+numbers='match'
+print(numbers[1:-1])
+first='John'
+last='Smith'
+msg=f'{first} {last} is a coder'
+print(msg)
+#arithmetic operations
+print(10/3)
+print(10//3)
+print(10%3)
+print(10*3)
+print(10**3)
+#operator precedence
+x=2+4*8**3/4
+print(x)
+x=2.9
+print(round(x))
+print(abs(-2.9))
+#tuples
+numbers=(1,2,3)
+print(numbers[0])
+#lists
+number1=[1,2,5,7,3]
+number2=number1.copy()
+print(number2)
+number1.sort()
+print(number1)
